f1b77a423e5fb71738fe6fb7d70f9d25fded3030
Patricio Fischi <patofinschi@gmail.com>
2024-10-16 22:28:23 -0300

    Ajustes header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { DatosService } from 'src/app/servicios/datos.service';
 import { AuthService } from 'src/app/servicios/auth.service';
@@ -8,22 +9,24 @@ import { AuthService } from 'src/app/servicios/auth.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent  implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   nombre: string = '';
-  private sharedService = inject(DatosService);
-
   direccion: string = '';
+  private sharedService = inject(DatosService);
   private authService = inject(AuthService);
-
+  private router = inject(Router);
   subscriptionDatos: Subscription = new Subscription();
   subscriptionAuthService: Subscription = new Subscription();
+  subscriptionRouter: Subscription = new Subscription(); // Suscripción al router
+  isLogin: boolean = false; // Variable para almacenar el estado de login
 
   constructor() { }
 
   ngOnInit() {
-    this.subscriptionDatos = this.sharedService.nombre$.subscribe(nombre =>{
-      this.nombre = nombre
+    // Suscribirse a los cambios de dirección y nombre
+    this.subscriptionDatos = this.sharedService.nombre$.subscribe(nombre => {
+      this.nombre = nombre;
       console.log('Header: ', nombre);
     });
 
@@ -31,10 +34,19 @@ export class HeaderComponent  implements OnInit {
       this.direccion = direccion;
       console.log('Header: ', direccion);
     });
+
+    // Suscribirse a los cambios de la ruta
+    this.subscriptionRouter = this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Verificar si estamos en la página principal ('/') o en la de login ('/login')
+        this.isLogin = this.router.url === '/' || this.router.url === '/login';
+      }
+    });
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.subscriptionDatos?.unsubscribe();
     this.subscriptionAuthService?.unsubscribe();
+    this.subscriptionRouter?.unsubscribe(); // Desuscribirse de la suscripción del router
   }
 }
