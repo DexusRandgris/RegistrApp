9caadada9caa7279e9c341182225663436d414cb
Patricio Fischi <patofinschi@gmail.com>
2024-10-16 22:58:10 -0300

    Revert "Se implementan alertas en recuperar contraseña"
    This reverts commit 5808e16e0558dc1648afb2d42f566b15e0f836e4.

--- src/app/views/recoverpw/recoverpw.component.ts
+++ src/app/views/recoverpw/recoverpw.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recoverpw',
@@ -8,40 +7,7 @@ import { AlertController } from '@ionic/angular';
 })
 export class RecoverpwComponent  implements OnInit {
 
-  private alertController = inject(AlertController);
-
-  direccion: string = '';
-  errorMessage: string = '';
-  successMessage: string = '';
-
   constructor() { }
-
-  async recuperar(direccion: string){
-    if(direccion === ''){
-      this.errorMessage = 'Debe ingresar su dirección de correo electrónico';
-      await this.mostrarAlerta('Error',this.errorMessage)
-      return;
-    }
-    else{
-      this.successMessage = 'Se ha enviado un correo de recuperación a su dirección de correo electrónico';
-      await this.mostrarAlerta('Recuperación de contraseña',this.successMessage);
-      this.direccion = '';
-      this.errorMessage = '';
-      this.successMessage = '';
-    }
-  }
-
-
-  async mostrarAlerta(header: string, message: string) {
-    const alert = await this.alertController.create({
-      header: header,
-      message: message,
-      buttons: ['OK']
-    });
-
-    await alert.present();
-  }
-
 
   ngOnInit() {}
 
